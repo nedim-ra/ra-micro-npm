18acc92ab2f6c044b8cc90203bc904468574607b
nedim hadzialic <n.hazialic@freelancer.ra-micro.de>
2024-09-06 04:16:25 +0200

    feat(core)!: Removed capitalize utility, testing automatic major version change

--- lib/main.ts
+++ lib/main.ts
@@ -2,5 +2,4 @@
 export { default as ComponentA } from "./components/ComponentA";
 
 // Export utility functions
-export { capitalize } from "./utils/capitalize";
 export { randomNumber } from "./utils/randomNumber";
